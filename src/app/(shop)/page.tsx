f7cd35cbedac8a7820f5e9d8050b0521f157d926
Aleksandr Raiko <awisenone@gmail.com>
2026-03-04 19:40:30 +0200

    chore(tailwind): update tailwind to v4

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,9 +1,3 @@
 export default async function Home() {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold">
-        Welcome to the shop! This is the main page of the shop section.
-      </h1>
-    </div>
-  )
+  return <div className='container'></div>
 }
