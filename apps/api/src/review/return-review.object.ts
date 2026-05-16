1046c35e3c1f586a183c53b7914eabf407fb5c74
Aleksandr Raiko <awisenone@gmail.com>
2026-05-16 17:48:49 +0300

    feat(api/review): implement CRUD endpoints, add DTO and return object

---
+++ apps/api/src/review/return-review.object.ts
@@ -0,0 +1,12 @@
+import { returnUserObject } from "@/user/return-user.object";
+import { Prisma } from "generated/prisma/client";
+
+export const returnReview: Prisma.ReviewSelect = {
+  id: true,
+  createdAt: true,
+  rating: true,
+  text: true,
+  user: {
+    select: returnUserObject
+  }
+}
--- apps/api/src/review/review.controller.ts
+++ apps/api/src/review/review.controller.ts
@@ -1,7 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Auth } from '@/auth/decorators/auth.decorator';
+import { CurrentUser } from '@/auth/decorators/user.decorator';
+import { Body, Controller, Get, Param, ParseIntPipe, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { ReviewDto } from './review.dto';
 import { ReviewService } from './review.service';
 
-@Controller('review')
+@Controller('reviews')
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
+
+  @UsePipes(new ValidationPipe())
+  @Get()
+  async getAll() {
+    return this.reviewService.getAll()
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Post('product/:productId')
+  @Auth()
+  async createReview(
+    @CurrentUser('id') id: number,
+    @Body() dto: ReviewDto,
+    @Param('productId', ParseIntPipe) productId: number,
+  ) {
+    return this.reviewService.create(id, dto, productId)
+  }
 }
