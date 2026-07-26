c14a84d22ab1b5c2ec84dc448c04a7d712afea3c
Aleksandr Raiko <awisenone@gmail.com>
2026-07-26 20:20:00 +0300

    feat(web): add proxy (middleware)

---
+++ apps/web/src/proxy.ts
@@ -0,0 +1,27 @@
+import { type NextRequest, NextResponse } from 'next/server'
+import { EnumTokens } from '@/features/auth/lib/auth.helpers'
+import { PUBLIC_URL } from '@/shared/config/url.config'
+
+export async function proxy(request: NextRequest) {
+  const refreshToken = request.cookies.get(EnumTokens.REFRESH_TOKEN)?.value
+
+  const isAuthPage = request.url.includes(PUBLIC_URL.auth())
+
+  if (isAuthPage) {
+    if (refreshToken) {
+      return NextResponse.redirect(new URL(PUBLIC_URL.user(), request.url))
+    }
+
+    return NextResponse.next()
+  }
+
+  if (refreshToken === undefined) {
+    return NextResponse.redirect(new URL(PUBLIC_URL.auth(), request.url))
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/user/:path*', '/auth', '/admin'],
+}
